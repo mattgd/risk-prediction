d4b130bb240ae86b9dc303519a5093bc77f685d9
jdschert <jdschert@NIPAH.localdomain>
2018-11-27 19:08:13 -0500

    Calculate accuries with test and train

--- predictor_comparison.py
+++ predictor_comparison.py
@@ -4,6 +4,7 @@ import preprocessor as pp
 import pandas as pd
 import numpy as np
 import sys
+from sklearn.metrics import r2_score as accuracy
 
 RAW_DATA = 'survey_responses.csv' 
 
@@ -23,4 +24,52 @@ if __name__ == '__main__':
 
     # Repeatedly run comparison of predictors
     else:
-        pass
+        ml_accuracies = []
+        ag_accuracies = []
+        ml_offness = []
+        ag_offness = []
+        for i in range(10):
+
+            # Split into test and train data
+            msk = np.random.rand(len(data)) < .8
+            train = data[msk]
+            test = data[~msk]
+            expected_ml = list(test.iloc[:,0])
+            expected_ag = list(expected_ml)
+            actual_input = test.iloc[:,1:]
+
+            # Predict with both predictors
+            p_ml = ml.MlPredictor(train)
+            p_ag = ag.AgentPredictor(train)
+            actual_ml = p_ml.predict(actual_input)
+            actual_ag = p_ag.predict(actual_input)
+            if any(isinstance(x, str) for x in actual_ag):
+                idxs = [i for i, x in enumerate(actual_ag) if isinstance(x, str)]
+                for idx in reversed(idxs):
+                    actual_ag.pop(idx)
+                    expected_ag.pop(idx)
+
+            # Calculate Accurcies
+            acc_ml = accuracy(expected_ml, actual_ml)
+            acc_ag = accuracy(expected_ag, actual_ag)
+            ml_accuracies.append(acc_ml)
+            ag_accuracies.append(acc_ag)
+            off_ml = np.mean([abs(x[0]-x[1]) for x in zip(expected_ml, actual_ml)])
+            off_ag = np.mean([abs(x[0]-x[1]) for x in zip(expected_ag, actual_ag)])
+            ml_offness.append(off_ml)
+            ag_offness.append(off_ag)
+
+            # Report
+            print("Run " + str(i))
+            print("\tMachine Learning Accuracy: " + str(acc_ml))
+            print("\tSimulated Agent Accuracy: " + str(acc_ag))
+            print("\tMachine Learning Offness: " + str(off_ml))
+            print("\tSimulated Agent Offness: " + str(off_ag))
+            print()
+
+        print("Overall Report")
+        print("\tMachine Learning Accuracy: " + str(np.mean(ml_accuracies)))
+        print("\tSimulated Agent Accuracy: " + str(np.mean(ag_accuracies)))
+        print("\tMachine Learning Offness: " + str(np.mean(ml_offness)))
+        print("\tSimulated Agent Offness: " + str(np.mean(ag_offness)))
+        print()
